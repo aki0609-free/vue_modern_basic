1b744e2c5ede1267c1d9aee5e570cb5ee12668c8
aki0609-free <akiyamafree0609@gmail.com>
2022-03-17 17:36:29 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
+import { User } from './types/types';
+import TheHeader from './components/TheHeader.vue';
+import BaseList from './components/BaseList.vue';
+import UserInfo from './components/UserInfo.vue';
+
+const activeUser = reactive<User>({
+  name: 'Maximillian Schwarzmuller',
+  description: 'Site owner and admin',
+  role: 'admin',
+});
 </script>
 
-<template></template>
+<template>
+  <TheHeader />
+  <BaseList />
+  <UserInfo
+    :full-name="activeUser.name"
+    :info-text="activeUser.description"
+    :role="activeUser.role"
+  />
+</template>
 
 <style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
 </style>
---
+++ src/components/UserInfo.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import BaseBadge from './BaseBadge.vue';
+type Props = {
+  fullName: string;
+  infoText: string;
+  role: 'admin' | 'author';
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <section>
+    <div>
+      <h3>{{ fullName }}</h3>
+      <BaseBadge :type="role" :caption="role.toUpperCase()" />
+    </div>
+    <p>{{ infoText }}</p>
+  </section>
+</template>
+
+<style scoped>
+section {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+section div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
